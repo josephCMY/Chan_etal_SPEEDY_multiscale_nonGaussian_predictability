e59fd98762646be5cdcd4c3d517b4867a92d67f1
JosephChan <chan.1063@osu.edu>
2025-08-04 16:32:51 -0400

    adding script used for data extraction

--- extract_surface_pressure_data.sh
+++ extract_surface_pressure_data.sh
@@ -5,35 +5,35 @@ src_dir=ens_data
 
 
 
-# date
-# echo EXTRACT DATA FOR EVERY OTHER DAY
-# echo ""
+date
+echo EXTRACT DATA FOR EVERY FIVE DAYS
+echo ""
 
 
-# # Extract surface pressure data for every other day
-# day_num=0
-# nday_interval=2
+# Extract surface pressure data for every other day
+day_num=0
+nday_interval=5
 
-# # Loop over all ensembles
-# for ensname in reference_ens perturbed_ens; do
+# Loop over all ensembles
+for ensname in reference_ens perturbed_ens; do
 
-#     for fname in `ls --color=none ens_data/$ensname/data_raw/2011*.nc`; do
+    for fname in `ls --color=none ens_data/$ensname/data_raw/2011*.nc`; do
 
-#         # Extract data
-#         if (( day_num % nday_interval == 0 )); then
-#             outfname=SPEEDY_ensemble_data/$ensname/data_raw/`basename $fname`
-#             ncks -v ps $fname -4 -O -L 1 $outfname
-#             echo Extracted   $fname   into   $outfname
-#         fi
+        # Extract data
+        if (( day_num % nday_interval == 0 )); then
+            outfname=SPEEDY_ensemble_data/$ensname/data_raw/`basename $fname`
+            ncks -v ps $fname -4 -O -L 1 $outfname
+            echo Extracted   $fname   into   $outfname
+        fi
 
-#         # Increment time counter
-#         (( day_num++ ))
+        # Increment time counter
+        (( day_num++ ))
 
-#     done
-# done
+    done
+done
 
 
-# echo "" 
+echo "" 
 
 
 
